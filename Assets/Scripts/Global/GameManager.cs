e65e23c8b59d7a147db78bb0c17462517e3d968c
jihye <thoth_0302@naver.com>
2024-01-25 16:28:44 +0900

    Feature - HP바

--- Assets/Scripts/Global/GameManager.cs
+++ Assets/Scripts/Global/GameManager.cs
@@ -36,6 +36,9 @@ public class GameManager : MonoBehaviour
 
 
     [Header("UI")]
+    public Image playerHP;
+
+
 
     //-----------------------------------------------------------------------------------------------
     //GameManager 에서 사용될 Component
