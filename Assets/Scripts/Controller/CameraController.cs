ff95e96777a6dd21c8c08d11aa5ab664b6e1ac83
jihye <thoth_0302@naver.com>
2024-01-25 11:09:33 +0900

    Fix - Camera 범위 조정
    맵 밖 부분 보이지 않도록 수정

--- Assets/Scripts/Controller/CameraController.cs
+++ Assets/Scripts/Controller/CameraController.cs
@@ -4,11 +4,31 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    private GameObject player;
+    private Transform player;
+    private float position_x;
+    private float position_y;
 
     void Update()
     {
-        player = GameManager.instance.player;
-        transform.position = new Vector3(player.transform.position.x, player.transform.position.y, transform.position.z);
+        player = GameManager.instance.player.transform;
+
+        if(player.position.x < -52 || player.position.x > 52)
+        {
+            position_x = 52 * ((player.position.x > 0) ? 1: -1);
+        }
+        else
+        {
+            position_x = player.position.x;
+        }
+        if(player.position.y < -35 || player.position.y > 35)
+        {
+            position_y = 35 * ((player.position.y > 0) ? 1 : -1);
+        }
+        else
+        {
+            position_y = player.position.y;
+        }
+
+        transform.position = new Vector3(position_x, position_y, transform.position.z);
     }
 }
